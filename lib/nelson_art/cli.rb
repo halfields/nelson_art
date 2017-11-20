aa3c54bf5800765b1166fec805d948496caada44
halfields <hfields697@aol.com>
2017-11-20 11:51:49 -0600

    changes to my cli controller.Still some bugs to work out.

--- lib/nelson_art/cli.rb
+++ lib/nelson_art/cli.rb
@@ -2,26 +2,29 @@ class NelsonArt::CLI
 
 	def call
 		welcome
-		menu
-		goodbye
+		first_choice
 	end
 
 	def welcome
 		puts "Welcome to 'What's Happening at the Nelson'"
 		puts "Would you: 1. like to see the Nelson Art Gallery's schedule, or"
-		puts "					 2. like to see a listing of current and future exhibits, or"
-		puts "					 3. exit"
-		puts "Enter a number"
-		@first_choice = input.upcase
-		case input
+		puts "           2. like to see a listing of current and future exhibits, or"
+		puts "           3. exit"
+		print "Enter a number: "
+	end
+
+	def first_choice
+		choice = gets.strip.upcase
+		case choice
 		when "1"
 			nelson_schedule
 		when "2"
 			exhibit_list
 		when "3", "EXIT"
-			puts "Goodbye. Please come back for information on future Nelson Art Gallery exhibits"
+			goodbye
 		else
 			puts "I don't recognize your choice. Please enter a number, (1, 2, or 3)."
+			welcome
 		end
 	end
 
@@ -40,9 +43,20 @@ class NelsonArt::CLI
 		puts "Thanksgiving"
 	  puts "Christmas Eve"
 		puts "Christmas Day"
+		puts
+		puts "Enter 'back' to see the previous menu."
+		if gets.strip.upcase == "BACK"
+			welcome
+		end
 	end
 
 	def exhibit_list
+		puts "exhibit list"
+		welcome
+	end
+
+	def goodbye
+		puts "Goodbye. Please come back for information on future Nelson Art Gallery exhibits"
 	end
 end
 
