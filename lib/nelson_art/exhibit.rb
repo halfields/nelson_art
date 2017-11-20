f156fb3648d9d4f96938c83dbef59f2a4072a176
halfields <hfields697@aol.com>
2017-11-20 12:50:31 -0600

    added attr_accessors, a collector array, and an initialize method which gives values to the attributes

--- lib/nelson_art/exhibit.rb
+++ lib/nelson_art/exhibit.rb
@@ -1,2 +1,17 @@
 class NelsonArt::Exhibit
+
+	attr_accessor :name, :dates, :description, :cost, :location
+
+	@@all_exhibits
+
+	def initialize(exhibit_hash)
+		#add attributes to exhibit objects as the attributes for each exhibit are scraped from the individual
+		#exhibit pages. Then each exhibit is pushed to a collector array.
+		exhibit_hash.each do |attribute, value|
+			self.send("#{attribute}=", value)
+		end
+		@@all_exhibits << self
+	end
+
+	
 end
